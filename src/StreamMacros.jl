b774c477916b3124bcf978a73d8fc15cecff3191
Alvaro de Diego <dediego@ma.tum.de>
2020-05-04 13:41:40 +0200

    Added macros and examples
    Moved from CoherentStructures.jl

--- src/StreamMacros.jl
+++ src/StreamMacros.jl
@@ -1,5 +1,11 @@
 module StreamMacros
 
-greet() = print("Hello World!")
+using ModelingToolkit: Variable, Differential, simplified_expr,
+      expand_derivatives, Expression, Operation, Constant
+      
+export @define_stream, @velo_from_stream, @var_velo_from_stream,
+       @vorticity_from_stream
 
+include("macros.jl")
+	
 end # module
