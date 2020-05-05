d617ea4f0f3f4c66ddcebef9a3e69290bad0a689
Alvaro de Diego <dediego@ma.tum.de>
2020-05-05 17:04:12 +0200

    Channged imports

--- src/StreamMacros.jl
+++ src/StreamMacros.jl
@@ -1,8 +1,15 @@
 module StreamMacros
 
+import StaticArrays
+const SA = StaticArrays
+
+import ModelingToolkit
 using ModelingToolkit: Variable, Differential, simplified_expr,
       expand_derivatives, Expression, Operation, Constant
-      
+
+import DiffEqBase 
+const ODE=DiffEqBase
+
 export @define_stream, @velo_from_stream, @var_velo_from_stream,
        @vorticity_from_stream
 
