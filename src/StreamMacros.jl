327dfdc418daa726d85c7ef2b99d243a8c2c5525
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2021-02-19 18:03:38 +0100

    add better support for inplace vector fields

--- src/StreamMacros.jl
+++ src/StreamMacros.jl
@@ -11,7 +11,9 @@ const ODE = DiffEqBase
 export
     @define_stream,
     @velo_from_stream,
+    @velo!_from_stream,
     @var_velo_from_stream,
+    @var_velo!_from_stream,
     @vorticity_from_stream
 
 include("macros.jl")
